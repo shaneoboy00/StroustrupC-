50d9f9f47da7a1f64004a271dfdd867c809204d3
shaneoboy00 <mrsomethingor4no7h3r@protonmail.com>
2025-06-16 15:25:22 +0000

    Add existing files

---
+++ StroustrupCPP/ch2/tour.loop2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;    // make std names available without std::prefix
+
+bool accept2()
+{
+    cout << "Do you want to proceed (y or n)?\n";    // write question
+
+    char answer = 0;
+    cin >> answer;                                   // read answer
+
+    switch (answer) {
+    case 'y' :
+        return true;
+    case 'n' :
+        return false;
+    default:
+        cout << "I'll take that for a no.\n";
+        return false;
+    }
+}
+
+int main () { }
